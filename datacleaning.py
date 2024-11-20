c1e2b3b435748b306c3f538a49179e7053ad8e88
Sam <80631546+samtorr13@users.noreply.github.com>
2024-11-20 10:44:06 -0400

    lib
    añadidas las librerias base al entorno virtual

--- datacleaning.py
+++ datacleaning.py
@@ -7,5 +7,6 @@ def clean(df):
     df["YearsWithCurrManager"].fillna(0, inplace=True)
     df.columns = df.columns.str.replace("[{}()%:]",'',regex=True)
     df.drop(columns=["Over18"], inplace=True)
+    df.to_csv("analitycs.csv", index=False)
     
     return df
