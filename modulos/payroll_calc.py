a19bd50f74fd268778f8747460529b8fdddfd7ab
flavored.soda <armadaismael@gmail.com>
2024-11-21 21:31:02 -0400

    Update payroll_calc.py

--- modulos/payroll_calc.py
+++ modulos/payroll_calc.py
@@ -2,8 +2,6 @@ import pandas as pd
 from faker import Faker
 import random
 
-# ESTE CODIGO AUN NO FUNCIONA FALTA LA SUMA DE LAS HRS EXTRA
-
 # faker para generar datos falsos (creo)
 fake = Faker()
 
@@ -11,21 +9,24 @@ fake = Faker()
 def calculate_payroll(salary_monthly, days_worked):
     salary_daily = salary_monthly / 30
     salary_hourly = salary_daily / 8  
-    total_salary = salary_daily * days_worked     
+    total_salary = salary_daily * days_worked + extra    
+    
     HENv = 0.08  # Hrs extra nocturnas valor
     HBNv = 0.16  # Hrs bono nocturno valor
     HEDv = 0.27  # Hrs extra diurnas valor
-
-    HENc = 0  # cantidad HEN
-    HBNc = 0  # cantidad HBN
-    HEDc = 0  # cantidad HED
     
+    HEN = HENaux * HENv  
+    HBN = HBNaux * HBNv # Cálculos de pago por hrs extra
+    HED = HEDaux * HEDv  
+    extra = HEN + HBN + HED
+
     IVSS = 2.24
-    RPE = 0.43
+    RPE = 0.43  # Retenciones varias
     FAOV = 0.86
+    
     tax = IVSS + RPE + FAOV
     total_to_pay = total_salary - tax
-    return salary_daily, salary_hourly, HEN, HBN, HED, total_salary, tax, total_to_pay
+    return salary_daily, salary_hourly, HENv, HBNv, HEDv, total_salary, tax, total_to_pay
 
 # lista para guardar datos
 data = []
@@ -36,9 +37,9 @@ def excel_gen():
         salary_monthly = fake.random_int(min=130, max=500)  # Random sal mensual
         days_worked = fake.random_int(min=20, max=30)  # Random dias trabajados
     
-        HBN = fake.random_int(min=0, max=2)
-        HED = fake.random_int(min=0, max=2)
-        HEN = fake.random_int(min=0, max=2)
+        HBNaux = fake.random_int(min=0, max=2)
+        HEDaux = fake.random_int(min=0, max=2)
+        HENaux = fake.random_int(min=0, max=2)
     
         # calcular valores de la nómina
         salary_daily, salary_hourly, total_salary, deductions, total_to_pay = calculate_payroll(salary_monthly, days_worked)
@@ -50,17 +51,14 @@ def excel_gen():
             "Fecha de Ingreso": fake.date_between(start_date='-3y', end_date='today'),
             "Salario Mensual": salary_monthly,
             "Salario Básico Diario": salary_daily,
-            "Salario por Hora": 
-            salary_hourly,
+            "Salario por Hora": salary_hourly,
             "Días Trabajados": days_worked,
-            "HEN": HEN,
-            "HBN": HBN,
-            "HED": HED,
-
-            "Cantidad HEN": 
-            "Cantidad HBN": 
-            "Cantidad HED": 
-            
+            "HEN": HENv,
+            "HBN": HBNv,
+            "HED": HEDv,
+            "Cantidad HEN": HENaux,
+            "Cantidad HBN": HBNaux,
+            "Cantidad HED": HEDaux,
             "Total Salario": total_salary,
             "IVSS": IVSS,
             "RPE": RPE,
@@ -74,5 +72,3 @@ def excel_gen():
     df.to_excel('nomina.xlsx', index=False)
 
     print("Dataset guardado en 'nomina.xlsx'.")
-
-excel_gen()
